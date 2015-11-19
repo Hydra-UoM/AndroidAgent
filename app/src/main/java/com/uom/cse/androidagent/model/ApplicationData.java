1e416912409a6fc3be1baa72932e84f2df9f7884
Sirojan Tharmakulasingam <sirojan.11@cse.mrt.ac.lk>
2015-11-19 20:30:24 +0530

    db model added

---
+++ app/src/main/java/com/uom/cse/androidagent/model/ApplicationData.java
@@ -0,0 +1,101 @@
+package com.uom.cse.androidagent.model;
+
+/**
+ * Created by Sirojan on 11/19/2015.
+ */
+public class ApplicationData {
+
+    private int _id;
+
+    private String _applicationName;
+
+    private String _packageName;
+
+    private String _averageCPU;
+
+    private String _averagePrivateMemoryUsage;
+
+    private String _averageSharedMemoryUsage;
+
+    private String _averageSentData;
+
+    private String _averagereceivedData;
+
+    public ApplicationData(int id,String applicationName,String packageName,String averageCPU,String averagePrivateMemoryUsage,String averageSharedMemoryUsage,String averageSentData,String averagereceivedData){
+        _id = id;
+        _applicationName = applicationName;
+        _packageName = packageName;
+        _averageCPU = averageCPU;
+        _averagePrivateMemoryUsage = averagePrivateMemoryUsage;
+        _averageSharedMemoryUsage = averageSharedMemoryUsage;
+        _averageSentData = averageSentData;
+        _averagereceivedData = averagereceivedData;
+    }
+
+
+    public String get_applicationName() {
+        return _applicationName;
+    }
+
+    public void set_applicationName(String _applicationName) {
+        this._applicationName = _applicationName;
+    }
+
+    public String get_averageCPU() {
+        return _averageCPU;
+    }
+
+    public void set_averageCPU(String _averageCPU) {
+        this._averageCPU = _averageCPU;
+    }
+
+
+
+    public String get_averagePrivateMemoryUsage() {
+        return _averagePrivateMemoryUsage;
+    }
+
+    public void set_averagePrivateMemoryUsage(String _averagePrivateMemoryUsage) {
+        this._averagePrivateMemoryUsage = _averagePrivateMemoryUsage;
+    }
+
+    public String get_averageSharedMemoryUsage() {
+        return _averageSharedMemoryUsage;
+    }
+
+    public void set_averageSharedMemoryUsage(String _averageSharedMemoryUsage) {
+        this._averageSharedMemoryUsage = _averageSharedMemoryUsage;
+    }
+
+    public String get_averageSentData() {
+        return _averageSentData;
+    }
+
+    public void set_averageSentData(String _averageSentData) {
+        this._averageSentData = _averageSentData;
+    }
+
+    public String get_averagereceivedData() {
+        return _averagereceivedData;
+    }
+
+    public void set_averagereceivedData(String _averagereceivedData) {
+        this._averagereceivedData = _averagereceivedData;
+    }
+
+    public String get_packageName() {
+        return _packageName;
+    }
+
+    public void set_packageName(String _packageName) {
+        this._packageName = _packageName;
+    }
+
+    public int getId() {
+        return _id;
+    }
+
+    public void setId(int id) {
+        this._id = id;
+    }
+}
---
+++ app/src/main/java/com/uom/cse/androidagent/model/DatabaseHandler.java
@@ -0,0 +1,8 @@
+package com.uom.cse.androidagent.model;
+
+/**
+ * Created by Sirojan on 11/19/2015.
+ */
+public class DatabaseHandler {
+
+}
